5e4428dab4b877cbaab6af363faedb8287b921eb
Francisco Knebel <franciscopaivaknebel@gmail.com>
2017-07-22 04:35:57 -0300

    Episode 16.

---
+++ ep/16/hello-vue/src/components/Message.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="box">
+    <p>
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css">
+  .box {
+    background: #c3c3c3;
+    padding: 1em;
+    margin: .3em;
+    border: 1px solid #3B3B3B;
+  }
+
+</style>
